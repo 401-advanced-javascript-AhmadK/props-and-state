152834a980b0f0fc75f232a4a9dfe60c034ee74a
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2020-02-29 16:35:38 +0200

    Fixed the JSON format

--- src/components/Results/results.js
+++ src/components/Results/results.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import ReactJSon from 'react-json-view';
 
 function Results (props){
   if (props.response.Headers) {
     return (
       <section className='results'>
-        <p>{(JSON.stringify(props.response ,null , '\t'))}</p>
+        <ReactJSon src = {props.response} />
       </section>
     );
   }else return <section></section>;
